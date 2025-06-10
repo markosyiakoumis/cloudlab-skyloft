e28223e1f6ec0432a4468b4b49d0ae7877424a98
Μάρκος Γιακουμής <slowmarc@tuta.io>
2025-06-10 18:13:43 +0300

    Add support for skyloft kmod

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+if [ -f /local/startup_service_done/ ]; then
+    apt-get update
+    apt-get install -y cmake pkg-config libdpdk-dev
+
+    git clone https:///github.com/yhtzd/skyloft /local/skyloft
+    cd /local/skyloft
+
+    make kmod
+    make insmod
+else
+    sed -i "s/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"isolcpus=0,1,2 /" /etc/default/grub
+    update-grub
+
+    > /local/startup_service_done
+    reboot
+fi
--- profile.py
+++ profile.py
@@ -6,22 +6,18 @@ Instructions:
 Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item. 
 """
 
-# Import the Portal object.
 import geni.portal as portal
-# Import the ProtoGENI library.
 import geni.rspec.pg as pg
 
-# Create a portal context.
 pc = portal.Context()
 
-# Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
  
-# Add a raw PC to the request.
 node = request.RawPC("node")
+node.hardware_type = "c6620"
+node.component_manager_id = "urn:publicid:IDN+utah.cloudlab.us+authority+cm"
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="echo test > /local/repository/test.txt"))
+node.addService(pg.Execute(shell="sh", command="/local/repository/init.sh"))
 
-# Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
