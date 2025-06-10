a07e3ba8761ca6a09dca626247068879d3b9a17f
Μάρκος Γιακουμής <slowmarc@tuta.io>
2025-06-10 19:26:34 +0300

    Fix kmod grub flag

--- init.sh
+++ init.sh
@@ -12,7 +12,7 @@ if [ -f /local/startup_service_done ]; then
     make kmod
     make insmod
 else
-    sed -i "s/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"isolcpus=0,1,2 /" /etc/default/grub
+    sed -i "s/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"isolcpus=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21 /" /etc/default/grub
     update-grub
 
     touch /local/startup_service_done
