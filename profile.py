f18b1537bb34301036f818f2d42c4acda65a9c0e
Μάρκος Γιακουμής <slowmarc@tuta.io>
2025-06-12 17:31:46 +0300

    Add client

--- profile.py
+++ profile.py
@@ -8,8 +8,12 @@ server = request.RawPC("server")
 server.hardware_type = "c6620"
 server.component_manager_id = "urn:publicid:IDN+utah.cloudlab.us+authority+cm"
 server.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
-server.addService(pg.Execute(shell="sh", command="/local/repository/server.sh"))
+server.addService(pg.Execute(shell="sh", command="/local/repository/scripts/server.sh"))
 
 client = request.RawPC("client")
+client.hardware_type = "c220g1"
+client.component_manager_id = "urn:publicid:IDN+wisc.cloudlab.us+authority+cm"
+client.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
+client.addService(pg.Execute(shell="sh", command="/local/repository/scripts/client.sh"))
 
 pc.printRequestRSpec(request)
---
+++ scripts/client.sh
@@ -0,0 +1,8 @@
+# Download Shenango
+git clone https://github.com/yhtzd/shenango-client.git /local/shenango-client
+cd /local/shenango-client
+
+# Download and Build DPDK
+./dpdk.sh
+
+
