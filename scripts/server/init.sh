fa8e5c34377f3b1e21c212a4c0a758cba8ef490d
Μάρκος Γιακουμής <slowmarc@tuta.io>
2025-06-13 16:15:00 +0300

    Debug perms

--- scripts/server/init.sh
+++ scripts/server/init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+whoami
+id
+
 # Install dependencies.
 apt-get update
 apt-get install -y git cmake pkg-config flex bison meson libelf-dev libssl-dev libnuma-dev python3 python3-numpy pyton3-matplotlib python3-pyelftools
 
 # Execute other necessary initialization scripts.
-bash -x kernel.sh
-bash -x dpdk.sh
-bash -x skyloft.sh
-bash -x setup-hosts.sh
-bash -x kmod.sh
+bash -x /local/repository/scripts/server/kernel.sh
+bash -x /local/repository/scripts/server/dpdk.sh
+bash -x /local/repository/scripts/server/skyloft.sh
+bash -x /local/repository/scripts/server/setup-hosts.sh
+bash -x /local/repository/scripts/server/kmod.sh
