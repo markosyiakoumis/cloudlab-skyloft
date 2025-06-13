4fa8b910aa7de14cb94d8b6c8bc47d9962458cf5
Μάρκος Γιακουμής <slowmarc@tuta.io>
2025-06-13 10:21:45 +0300

    Misc changes

--- scripts/client.sh
+++ scripts/client.sh
@@ -1,8 +1,9 @@
+apt-get update
+apt-get install libnuma-dev
+
 # Download Shenango
 git clone https://github.com/yhtzd/shenango-client.git /local/shenango-client
 cd /local/shenango-client
 
 # Download and Build DPDK
 ./dpdk.sh
-
-
